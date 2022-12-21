4627df79805d6ca70a766d379e3d47a67cf662bb
ihor000 <ihor.silnov@yandex.ru>
2022-12-22 02:00:39 +0300

    text

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите колличество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
